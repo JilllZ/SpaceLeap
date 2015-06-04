2f82dad25de75a792ac40ec9265eca61c41345b4
Amarcolina <Amarcoli@ucsc.edu>
2015-06-04 13:36:55 -0700

    Initial import of dynamic cable script

---
+++ Assets/Scripts/Editor/DynamicCableEditor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(DynamicCable))]
+public class DynamicCableEditor : Editor {
+
+    public void OnSceneGUI() {
+        if (!Application.isPlaying) {
+            DynamicCable dynamicCable = (DynamicCable)target;
+
+            dynamicCable.updateListSizes();
+
+            for (int i = 0; i <= dynamicCable.wires; i++) {
+                dynamicCable.anchorPoints[i] = Handles.PositionHandle(dynamicCable.anchorPoints[i] + dynamicCable.transform.position, Quaternion.identity) - dynamicCable.transform.position;
+            }
+
+            dynamicCable.updateMesh();
+        }
+    }
+}
