96d4cbea2d4c2ff2f88d5976fc7f4eccc689ce4c
Amarcolina <Amarcoli@ucsc.edu>
2015-06-05 13:45:41 -0700

    working on widgets

---
+++ Assets/Scripts/Panels/ToggleButtonPanel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using LMWidgets;
+
+public class ToggleButtonPanel : InteractionObject, IDataBoundWidget<ButtonToggleBase, bool> {
+    public Text label;
+    public ButtonToggleBase button;
+
+    void Start() {
+
+    }
+}
