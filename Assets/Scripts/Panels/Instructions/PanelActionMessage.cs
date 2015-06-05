f45663bf68b5a80b89e8767ba1656b215903f418
Amarcolina <Amarcoli@ucsc.edu>
2015-06-04 18:01:46 -0700

    Added panel action message

---
+++ Assets/Scripts/Panels/Instructions/PanelActionMessage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Message is sent from client to server whenever the user performs an 
+ * action on a panel.  Needs to be sent at all time so that the server 
+ * does not choose an instruction that has already been followed
+ */
+public class PanelActionMessage : CustomMessage {
+    public int setId;
+    public int variantNumber;
+
+    public PanelActionMessage(int setId, int variantNumber) {
+        this.setId = setId;
+        this.variantNumber = variantNumber;
+    }
+}
